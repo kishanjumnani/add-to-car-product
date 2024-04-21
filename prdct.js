811cae32b43cdd823ba2fa6cd4f6f245b9ead668
kishanjumnani <kishanimpjumnani@gmail.com>
2024-04-21 23:17:29 +0530

    s

---
+++ prdct.js
@@ -0,0 +1,30 @@
+let prdct =[
+    {
+        id:1,
+        name:"Apple",
+        price:1000,
+
+    },
+    {
+        id:2,
+        name:"Banana",
+        price:2000,
+    },
+    {
+        id:3,
+        name:"Orange",
+        price:3000,
+    }
+]
+
+ const items = document.querySelector(".items");
+//  prdct.forEach(function(prdct){
+//      items.innerHTML += `<div class="item">
+//      <h5>${prdct.name}</h5>
+//      <h5>${prdct.price}</h5>
+
+//      <button class="add-to-cart-btn">Add to cart</button>
+//      </div>`
+//  });
+
+ document.createElement("rows");
